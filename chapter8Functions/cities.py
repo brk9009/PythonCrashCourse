b3b4798e59eccee17cceba77c45b3ad085d83128
brk9009 <warsend23@hotmail.com>
2018-11-08 02:24:40 -0600

    eclipse test

---
+++ chapter8Functions/cities.py
@@ -0,0 +1,7 @@
+#8.5 Python Crash Course
+def describe_city(city_name, country_name='USA'):
+    print(city_name.title() + " is in the " + country_name)
+
+describe_city('Walnut')
+describe_city('Atlantic')
+describe_city('Berlin')
