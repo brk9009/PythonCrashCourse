4ccea6b50eb2a0737f96b8243d8443f541c09ef8
brk9009 <warsend23@hotmail.com>
2019-03-24 19:16:00 -0500

    edit topic and entry

--- django/exercises/meal_planner/meal_plans/views.py
+++ django/exercises/meal_planner/meal_plans/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    """The home page for Learning Log"""
+    """The home page for Meal plans"""
     return render(request, 'meal_plans/index.html')
