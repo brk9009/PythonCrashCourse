70a1f3632927b38e7de02aa342e0739c00a03837
brk9009 <warsend23@hotmail.com>
2019-04-07 22:59:23 -0500

    error codes

--- django/learning_log/learning_logs/views.py
+++ django/learning_log/learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -20,7 +20,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     # Make sure the topic belongs to the current user.
     check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
@@ -48,7 +48,7 @@ def new_topic(request):
 @login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     if request.method != 'POST':
         # No data submitted; create a blank form
         form = EntryForm()
@@ -68,7 +68,7 @@ def new_entry(request, topic_id):
 @login_required    
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
-    entry = Entry.objects.get(id=entry_id)
+    entry = get_object_or_404(Entry, id=entry_id)
     topic = entry.topic
     check_topic_owner(topic, request)
 
