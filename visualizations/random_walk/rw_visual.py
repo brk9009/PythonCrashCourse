62d3e5a85aca2aced7d5ea66faca058d3b857a74
brk9009 <warsend23@hotmail.com>
2019-04-09 00:44:47 -0500

    random walk exercises

--- visualizations/random_walk/rw_visual.py
+++ visualizations/random_walk/rw_visual.py
@@ -12,18 +12,21 @@ while True:
     plt.figure(figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-        edgecolors='none', s=1)
+    # plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+
+    # For a linegraph    
+    plt.plot(rw.x_values, rw.y_values, linewidth=5)
 
     # Emphasize the first and last points.
-    plt.scatter(0,0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.scatter(0,0, c='green', edgecolors='none', s=500)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=500)
 
     # Remove the axes
     plt.gca().get_xaxis().set_visible(False)
     plt.gca().get_yaxis().set_visible(False)
 
-    plt.savefig('random_walk_scatter.png', bbox_inches='tight')
+    # plt.show()
+    plt.savefig('random_walk_positive.png', bbox_inches='tight')
 
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
